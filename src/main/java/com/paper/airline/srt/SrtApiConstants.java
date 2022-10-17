3650cd922d9a7297af71c614f556999c29ebabcb
jun94.jang <stvens@naver.com>
2022-10-17 22:39:49 +0900

    상수 수정

--- src/main/java/com/paper/airline/srt/SrtApiConstants.java
+++ src/main/java/com/paper/airline/srt/SrtApiConstants.java
@@ -5,5 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class SrtApiConstants {
     public static final String SRT_LOGIN_URL = "https://etk.srail.kr/cmc/01/selectLoginForm.do";
-    public static final String SRT_SCHEDULE_URL = "https://etk.srail.kr/hpg/hra/01/selectScheduleList.do?";
+    public static final String SRT_SCHEDULE_URL = "https://etk.srail.kr/hpg/hra/01/selectScheduleList.do";
 }
